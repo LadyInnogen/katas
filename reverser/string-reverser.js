78a56c17f7684bdb32c1428d16103b5393479e4e
Jenny <ladyinnogen@gmail.com>
2019-02-27 19:13:27 -0600

    Refactor(string-reverser): Export string reverser function

--- reverser/string-reverser.js
+++ reverser/string-reverser.js
@@ -1,7 +1,18 @@
-let reversedMessage = "";
-let message = "Call me Ishmael";
-for(let i=1; i<=message.length; i++){
-	//Each iteration, grab each letter from the end to the beginning
-	reversedMessage += message[message.length-i];
-}
-console.log("Secret Message: ", reversedMessage);
+
+let messageToReverse = "Call me Ishmael";
+
+function reverse(message){
+	let reversedMessage = "";
+	for(let i=1; i<=message.length; i++){
+		//Each iteration, grab each letter from the end to the beginning
+		reversedMessage += message[message.length-i];
+	}
+	return reversedMessage;
+};
+
+console.log("Secret Message: ", reverse(messageToReverse));
+
+module.exports = {
+	reverse: reverse
+};
+
