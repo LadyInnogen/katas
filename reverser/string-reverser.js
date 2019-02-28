3cc78b2c2974f0923b22d883e5f9199cd64f906d
Jenny <ladyinnogen@gmail.com>
2019-02-27 18:51:20 -0600

    Fix(string-reverser): Print solution, include first letter, remove undefined

--- reverser/string-reverser.js
+++ reverser/string-reverser.js
@@ -1,7 +1,7 @@
 let reversedMessage = "";
 let message = "Call me Ishmael";
-for(let i=0; i<message.length; i++){
+for(let i=1; i<=message.length; i++){
 	//Each iteration, grab each letter from the end to the beginning
 	reversedMessage += message[message.length-i];
 }
-console.log("Secret Message: ", reverseMessage);
+console.log("Secret Message: ", reversedMessage);
