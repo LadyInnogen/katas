9fc518a75b188ae65d3ef1259bf3d6de45f8ad7a
Jenny <ladyinnogen@gmail.com>
2019-02-27 18:37:09 -0600

    Refactor(string-reverser): Invert loop logic to use incrementing instead of decrementing

--- reverser/string-reverser.js
+++ reverser/string-reverser.js
@@ -1,6 +1,7 @@
 let secretMessage = "";
 let message = "Call me Ishmael";
-for(let i=message.length-1; i>=0; i--){
-	secretMessage += message[i];
+for(let i=0; i<message.length; i++){
+	//Each iteration, grab each letter from the end to the beginning
+	secretMessage += message[message.length-i];
 }
 console.log("Secret Message: ", secretMessage);
